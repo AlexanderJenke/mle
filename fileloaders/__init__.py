1927b994bfc454e8c713879cd91dacca3931bba0
Jenke, Alexander <alexander.jenke@uniklinikum-dresden.de>
2020-12-04 14:58:54 +0100

    Add fileloaders.NIFII and fileloaders.DICOM

---
+++ fileloaders/__init__.py
@@ -0,0 +1,1 @@
+from .sitk import NIFTI, DICOM
---
+++ fileloaders/sitk.py
@@ -0,0 +1,53 @@
+import SimpleITK as sitk
+from numpy import ndarray, transpose
+from torch import Tensor
+
+
+class SITKFile:
+
+    def __init__(self, path: str, file: sitk.Image):
+        self.path = path
+        self.file = file
+        self.volume = sitk.GetArrayFromImage(self.file).transpose((1, 2, 0))
+        self.spacing = self.file.GetSpacing()
+
+    def write_data(self, volume: ndarray or Tensor, file_name: str):
+        """
+        Creates a file containing the given volume while copying the meta data from the called object
+        :param volume: image data to be writen to the file
+        :param output_file: path to the file to be written
+
+        """
+        if isinstance(volume, Tensor):
+            volume = volume.detach().cpu().numpy()
+        image = sitk.GetImageFromArray(transpose(volume, (2, 0, 1)))
+        image.CopyInformation(self.file)
+        sitk.WriteImage(image, file_name)
+
+
+class NIFTI(SITKFile):
+    """
+    SimpleITK Image from a file
+    """
+    def __init__(self, path):
+        """
+        Reads an image from a file
+        :param path: file to be read
+        """
+        super().__init__(path=path, file=sitk.ReadImage(path))
+
+
+class DICOM(SITKFile):
+    """
+    SimpleITK Image from a Series
+    """
+    def __init__(self, directory):
+        """
+        Reads an image from an image series
+        :param directory: directory containing the image series
+        """
+        reader = sitk.ImageSeriesReader()
+        dicom_names = reader.GetGDCMSeriesFileNames(directory)
+        reader.SetFileNames(dicom_names)
+        reader.MetaDataDictionaryArrayUpdateOn()
+        super().__init__(path=directory, file=reader.Execute())
