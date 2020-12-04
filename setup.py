424ee42252388d24438b57deff663e42c11d1e94
Jenke, Alexander <alexander.jenke@uniklinikum-dresden.de>
2020-12-04 13:01:03 +0100

    Add setup

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
+"""
+This setup file makes the package available to other projects
+"""
+
 import os
+
 import sys
 
 if __name__ == '__main__':
+    """
+    Creates a softlink to this package in the latest python site-package path.
+    """
     os.system(f"ln -sf -t {sys.path[-1]} $(pwd)")
